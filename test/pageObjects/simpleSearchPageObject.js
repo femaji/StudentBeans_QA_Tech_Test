c9bbf368adbe84b960a4c644625749fc9bd19b67
Femi Ajiboye <femajiboye@gmail.com>
2022-01-27 18:18:13 +0000

    Completed Test

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -9,6 +9,30 @@ class simpleSearchPageObject extends ParentPageObject {
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
   }
+  handleCookie() {
+    const cookieBtn = $("#onetrust-accept-btn-handler");
+    cookieBtn.waitForClickable();
+    cookieBtn.click();
+  }
+
+  openSearchBar() {
+    const input = $('[name="query"]');
+    input.waitForClickable();
+    input.click();
+  }
+
+  searchForBrand(brandName) {
+    const input = $("._1g5dvk1");
+    input.waitForClickable();
+    input.setValue(brandName);
+    expect(input.getValue()).to.equal(brandName);
+  }
+
+  validateSearchResults(brandName) {
+    browser.pause(3000);
+    const firstResult = $("._63p46ei");
+    expect(firstResult.getText()).to.equal(brandName);
+  }
 }
 
 module.exports = simpleSearchPageObject
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,9 +1,22 @@
-const { Given } = require('cucumber')
+const { Given, When, Then } = require('cucumber')
 const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
 const simpleSearchPageObject = new SimpleSearchPageObject()
 
-Given('I am on the studentbeans homepage', function () {
-  simpleSearchPageObject.goToHomePage()
-  simpleSearchPageObject.verifyHomePage()
-})
+Given("I am on the studentbeans homepage", function () {
+  simpleSearchPageObject.goToHomePage();
+  simpleSearchPageObject.verifyHomePage();
+});
+
+Given("I open the search bar", function () {
+  simpleSearchPageObject.handleCookie();
+  simpleSearchPageObject.openSearchBar();
+});
+
+When("I enter {string}", function (brandName) {
+  simpleSearchPageObject.searchForBrand(brandName);
+});
+
+Then("I should be shown a search listing for {string}", function (brandName) {
+  simpleSearchPageObject.validateSearchResults(brandName);
+});
